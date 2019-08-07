01e6034980a619039e561fd16b8c4c3432aecee2
MichelleYang2017 <michelleyang2017a@gmail.com>
2019-08-07 19:18:47 +0800

    modify  homework2 in stage1

--- stage1/hw2/hw2.py
+++ stage1/hw2/hw2.py
@@ -19,6 +19,24 @@
 """
 
 
+# 用户信息类
+class UserInfo():
+    def __init__(self, user_name, user_pwd, user_money):
+        self.name = user_name
+        self.pwd = user_pwd
+        self.money = user_money
+        self.shop_list = []
+        self.consume = 0
+
+    def shop_info(self, shop_list, consume):
+        for it in shop_list:
+            self.shop_list.append(it)
+        self.consume += consume
+        self.money -= consume
+        return self.shop_list, self.consume
+
+
+# 解决登陆时不存在该用户、注册时已存在用户的方法
 def other_choices():
     while True:
         choose = input("请输入您的选择==>").strip()
@@ -26,14 +44,15 @@ def other_choices():
             signup()
             break
         elif choose.isdigit() and int(choose) == 1:
-            login()
+            login_shop()
             break
         else:
             print("输入不合法，请重新选择,注册别名请选择0，登陆请选择1")
 
 
 # 登陆
-def login():
+def login_shop():
+    global user
     try_num = 3
     user_input_name = input('请输入已注册的用户名==>').strip()
     if user_input_name in black_list:
@@ -48,17 +67,20 @@ def login():
             line_user_name = line.split('|')[0]
             if line_user_name == user_input_name:
                 exist_flag = True
+                # 判断密码是否正确
                 while try_num:
                     user_input_pwd = input('请输入您的密码==>').strip()
                     try_num -= 1
                     if user_input_pwd == line.split('|')[1]:
                         wage_user = float(line.split('|')[2])
                         pwd_flag = True
+
+                        user = UserInfo(user_input_name, user_input_pwd, wage_user)
                         break
                 else:
-                    print('你输入密码有误，已经加入黑名单！您被迫退出')
+                    print('你输入密码有误，已经加入黑名单！您被迫重新做选择')
                     black_list.append(user_input_name)
-                    f_blck.write(user_input_name + '\n')
+                    f_black.write(user_input_name + '\n')
                     return
             if pwd_flag:
                 break
@@ -66,34 +88,13 @@ def login():
             print("该用户不存在，注册别名请选择0，登陆请选择1")
             other_choices()
 
+
+def shop():
     ##################登陆成功，可以购物了##############
-    print("恭喜您，登陆成功，请选择")
-    while True:
-        print("2 退出\t\t\t 3 购物")
-        choice = input('请选择==>').strip()
-        if choice.isdigit() and int(choice) == 2:
-            print('再见')
-            return
-        elif choice.isdigit() and int(choice) == 3:
-            wage, good_list = good_choice(wage_user)
-            print("您购买的商品如下:")
-            for it in good_list:
-                print(it)
-            print('您的余额为:', wage)
-            new_line = user_input_name + "|" + user_input_pwd + "|" + str(wage)
-            with open(file_path, 'r', encoding='utf-8') as f, open(new_file_path, 'w', encoding='utf-8') as f_new:
-                for line in f:
-                    if line.split('|')[0] == user_input_name:
-                        line = new_line
-                    f_new.write(line)
-
-            break
-        else:
-            print("不合法的输入，请重新祖选择！")
-
-
-def good_choice(wage):
+    name, pwd, wage = user.name, user.pwd, user.money
+    print("恭喜您，登陆成功，请选择商品")
     good_list = []
+    consume = 0
     while True:
         print("商品列表如下：")
         print(good_dict)
@@ -111,15 +112,16 @@ def good_choice(wage):
                 if wage < price:
                     print("工资不够，请重新选择商品:")
                 else:
-                    wage -= price
-                    print("商品%s已经加入购物车，您的余额为:%s" % (good, str(wage)))
+                    consume += price
+                    print("商品%s已经加入购物车，您的余额为:%s" % (good, str(wage - consume)))
                     content = "id", choice, "good：", good, 'price:', wage
                     good_list.append(content)
+
         elif choice == 'q':
             break
         else:
             print("不存在该商品！")
-    return wage, good_list
+    return consume, good_list
 
 
 # 注册
@@ -129,8 +131,9 @@ def signup():
     while True:
         user_name = input("请输入姓名==>").strip()
         if user_name in all_user_list:
-            print("该用户已注册，注册别名请选择0，登陆请选择1")
+            print("该用户已注册!请重新输入，注册别名请选择0，登陆请选择1")
             other_choices()
+            return
         else:
             break
     # 设置密码
@@ -147,7 +150,9 @@ def signup():
         all_user_list.append(user_name)
 
 
-if __name__ == '__main__':
+def main():
+    #  主逻辑
+    global good_dict, file_path, black_user, all_user_list, new_file_path, black_list, f_black
     good_dict = {
         '1': {"iphone": 8000},
         '2': {'macbook': 10000},
@@ -155,34 +160,74 @@ if __name__ == '__main__':
         '4': {'banana': 10},
         '5': {'juice': 20}
     }
+
     file_path = './user_info.txt'
     black_user = './black.txt'
+    # 加载黑名单的人
     black_list = []
     with  open(black_user, 'r', encoding='utf-8') as f1:
         for line in f1:
             black_list.append(line.split('\n')[0])
-    print(black_list)
-    f_blck = open(black_user, 'a+', encoding='utf-8')
+    f_black = open(black_user, 'a+', encoding='utf-8')
 
+    # 加载所有注册的用户
     all_user_list = []
-    try_num = 3
+    with  open(file_path, 'r', encoding='utf-8') as f1:
+        for line in f1:
+            name = line.split('|')[0]
+            all_user_list.append(name)
+
     new_file_path = './new_user_info.txt'
     print(
-        "欢迎光临该网站，请输入您的选择(例如1):\n "
+        "欢迎光临该网站，请输入您的选择(例如1): "
     )
+    global user
+    user = None
+    consume, shop_list = 0, []
     while True:
-        choice = input("请选择 0 注册 1 登陆 2 退出==>").strip()
+        choice = input("请选择 0 注册 1 登陆 2 购物 3退出==>").strip()
         if choice.isdigit():
             choice = int(choice)
+            # 注册
             if choice == 0:
                 signup()
+            # 登陆
             elif choice == 1:
-                login()
+                login_shop()
+            # 购物
             elif choice == 2:
-                print("再见！")
-                break
+                if not user:
+                    print('请先登录！')
+                else:
+                    # 购物并更新用户信息
+                    consume, shop_list = shop()
+                    shop_list, consume = user.shop_info(shop_list, consume)
+            # 退出
+            elif choice == 3:
+                if not user:
+                    print("您购买的商品如下:")
+                    for it in shop_list:
+                        print(it)
+                    print('您的余额为:', user.money - user.consume)
+                    new_line = user.name + "|" + user.pwd + "|" + str(user.money - user.consume)
+                    with open(file_path, 'r', encoding='utf-8') as f, open(new_file_path, 'w',
+                                                                           encoding='utf-8') as f_new:
+                        for line in f:
+                            if line.split('|')[0] == user.name:
+                                line = new_line
+                            f_new.write(line)
+
+                    print("再见！")
+                    break
+                else:
+                    print('好可惜，没有带走一件商品，欢迎您下次光临')
+                    break
             else:
                 print("选择不合法，请重新选择！")
         else:
             print("选择不合法，请重新选择！")
-    f_blck.close()
+    f_black.close()
+
+
+if __name__ == '__main__':
+    main()
